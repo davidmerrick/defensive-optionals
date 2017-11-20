d1150eb817a990b8b706b08721916a02b8e9e2b6
David Merrick <david@david-merrick.com>
2017-11-20 11:02:56 -0800

    Removed superfluous null check

--- src/main/java/com/davidmerrick/Cat.java
+++ src/main/java/com/davidmerrick/Cat.java
@@ -7,11 +7,13 @@ public class Cat {
     private Optional<LaserVision> laserVisionOptional = Optional.empty();
 
     public void setLaserVisionOptional(Optional<LaserVision> laserVisionOptional) {
-        if(laserVisionOptional == null){
-            this.laserVisionOptional = Optional.empty();
-        } else {
+        if(laserVisionOptional != null){
             this.laserVisionOptional = laserVisionOptional;
         }
+    }
+
+    public void setLaserVisionOptional(LaserVision laserVision) {
+        this.laserVisionOptional = Optional.ofNullable(laserVision);
     }
 
     public Optional<LaserVision> getLaserVisionOptional() {
